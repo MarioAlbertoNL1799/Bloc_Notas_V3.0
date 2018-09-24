b628551dd57a863f896c38dc93d37f954765c656
Mario Alberto Nieto López <37378931+MarioAlbertoNL1799@users.noreply.github.com>
2018-09-23 19:55:01 -0500

    Descripcion de la validacion de datos

--- src/extras/DataValidation.java
+++ src/extras/DataValidation.java
@@ -8,10 +8,16 @@ package extras;
 import javax.swing.JOptionPane;
 
 /**
- *
+ * Clase que permite la validacion de datos tomada de un jtf y comprobar que este pueda ser numerico
  * @author manl_
  */
 public class DataValidation {
+    /**
+     * Metodo que permiten el verificar que el valor de un jtf ingresado sea numerico
+     * y haga la conversion de valor cadena a un valor doble y de lo contrario envie mensaje de error.
+     * @param value valor del jtf que se recibe y sera verificado
+     * @return 
+     */
     public int string2Int(String value){
         int result = 0;
         try{
@@ -24,6 +30,12 @@ public class DataValidation {
         return result;
     }
     
+    /**
+     * Metodo que permiten el verificar que el valor de un jtf ingresado sea numerico
+     * y haga la conversion de valor cadena a un valor flotante y de lo contrario envie mensaje de error.
+     * @param value valor del jtf que se recibe y sera verificado
+     * @return 
+     */
     public float string2Float(String value){
         float result = 0;
         try{
@@ -36,6 +48,12 @@ public class DataValidation {
         return result;
     }   
     
+    /**
+     * Metodo que permiten el verificar que el valor de un jtf ingresado sea numerico
+     * y haga la conversion de valor cadena a un valor doble y de lo contrario envie mensaje de error.
+     * @param value valor del jtf que se recibe y sera verificado
+     * @return 
+     */
     public double string2Double(String value){
         double result = 0;
         try{
